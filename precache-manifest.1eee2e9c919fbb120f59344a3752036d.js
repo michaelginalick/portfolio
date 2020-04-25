1a67e6bad87365965b5b797a8b7c11890a839e1b
Michael <michael.ginalick@gmail.com>
2020-04-25 15:41:27 -0500

    Updates

---
+++ precache-manifest.1eee2e9c919fbb120f59344a3752036d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d66f710b4a976637957d30721fbff2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b5bb08de571472b28d09",
+    "url": "/portfolio/static/css/main.4a329334.chunk.css"
+  },
+  {
+    "revision": "cdea93c2ed2982aab5d4",
+    "url": "/portfolio/static/js/2.926fd429.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/portfolio/static/js/2.926fd429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5bb08de571472b28d09",
+    "url": "/portfolio/static/js/main.cb3cefaf.chunk.js"
+  },
+  {
+    "revision": "43c6552913f851b8fa47",
+    "url": "/portfolio/static/js/runtime-main.4cb11019.js"
+  },
+  {
+    "revision": "8ca0f87ad155a5da3753dd101730084b",
+    "url": "/portfolio/static/media/num.8ca0f87a.png"
+  },
+  {
+    "revision": "646a4579b99cc47db229d43f17a421f2",
+    "url": "/portfolio/static/media/star_search.646a4579.png"
+  }
+]);
